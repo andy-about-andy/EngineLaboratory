c3366f009d5b901b98ac7e04ca741cb1ace8dbdf
andrey_lutsevich <andrey-360.90@mail.ru>
2023-02-25 08:02:55 +0300

    Стилизацию

--- source/js/main.js
+++ source/js/main.js
@@ -1,26 +1,92 @@
-const tabsContentInner = document.querySelectorAll('.tabs__content-list--nojs');
-tabsContentInner.forEach((contentInner) => {
-  contentInner.classList.remove('tabs__content-list--nojs');
-});
+const headerMain = document.querySelector('.main-header');
+const navMain = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
+const mainBlock = document.querySelector('.main-block__wrap');
+const overflowMenu = document.querySelector('.main-nav__list');
+const headerInner = document.querySelector('.main-header__top-inner');
 
-document.querySelectorAll('.tabs__item').forEach((item) =>
-  item.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    const id = evt.target.getAttribute('href').replace('#', '');
+const textWorkingMode = document.querySelector('.main-header__contacts-text');
+const textPhone = document.querySelector('.main-header__link-phone');
+const textCall = document.querySelector('.main-header__button');
 
-    document.querySelectorAll('.tabs__item').forEach(
-        (child) => child.classList.remove('tabs__item--active')
-    );
-    document.querySelectorAll('.tabs__content-list').forEach(
-        (child) => child.classList.remove('tabs__content-list--active')
-    );
+headerMain.classList.remove('main-header--nojs');
+navMain.classList.remove('main-nav--nojs');
 
-    item.classList.add('tabs__item--active');
+// Функция закрытия меню
+const closesMenu = () => {
+  navMain.classList.add('main-nav--closed');
+  navMain.classList.remove('main-nav--opened');
+  headerInner.classList.remove('main-header__top-inner--opened');
+  textWorkingMode.style.color = '#ffffff';
+  textPhone.style.color = '#ffffff';
+  textCall.style.color = '#ffffff';
+  document.body.style.overflow = '';
+};
 
-    document.getElementById(id).classList.add('tabs__content-list--active');
-  })
-);
+const burger = () => {
+  navToggle.addEventListener('click', () => {
+    if (navMain.classList.contains('main-nav--closed')) {
+      navMain.classList.remove('main-nav--closed');
+      navMain.classList.add('main-nav--opened');
+      headerInner.classList.add('main-header__top-inner--opened');
+      textWorkingMode.style.color = '#282828';
+      textPhone.style.color = '#282828';
+      textCall.style.color = '#282828';
+      document.body.style.overflow = 'hidden';
+      overflowMenu.style.overflow = 'auto';
+    } else {
+      closesMenu();
+    }
+  });
+};
 
-const tabs = () => {
-  document.querySelector('.tabs__item').click();
+// Скрывает в mobile меню при нажатии на ссылку
+const hidesMenu = () => {
+  overflowMenu.addEventListener('click', () => {
+    if (navMain.classList.contains('main-nav--opened')) {
+      closesMenu();
+    }
+  });
+};
+
+burger();
+
+// маска для набора номера
+// Функция взята из интернета и доработана
+// Источник - https://webkaa.ru/javascript/maska-nomera-telefona-js
+
+const eventCallback = function (evt) {
+  let el = evt.target,
+  pattern = el.dataset.phonePattern,
+  matrixDefault = "+7(___) ___-__-__",
+  matrix = pattern ? pattern : matrixDefault,
+  i = 0,
+  def = matrix.replace(/\D/g, ""),
+  val = evt.target.value.replace(/\D/g, "");
+
+  if (def.length >= val.length) val = def;
+  evt.target.value = matrix.replace(/./g, function (a) {
+    return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a
+  });
+}
+const phoneInputs = document.querySelectorAll('[data-mask-tel]');
+for (let elem of phoneInputs) {
+  for (let ev of ['input', 'blur', 'focus']) {
+    elem.addEventListener(ev, eventCallback);
+  }
+}
+
+const button = document.querySelector('.main-header__button');
+const modal = document.querySelector('.modal');
+
+const popup = () => {
+  button.addEventListener('click', () => {
+    if (modal.classList.contains('modal--preload')) {
+      modal.classList.remove('modal--preload');
+      modal.classList.add('is-active');
+    } else {
+      modal.classList.add('modal--preload');
+      modal.classList.remove('is-active');
+    }
+  });
 };
